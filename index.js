9f989fb0bdee5ef1ee0f3d7b9820634843eeb89c
md. soliman <seo.mdsoliman@gmail.com>
2023-06-13 21:31:35 +0600

    add transition animation

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 
-/////////////////////////////////////////////////
+/////////////////////////   Paralox animation ////////////////////////
 document.addEventListener("mousemove",paralox);
 function paralox(e){
     document.querySelectorAll(".object").forEach(function(move){
@@ -11,4 +11,55 @@ function paralox(e){
         
         move.style.transform="translateX(" + x +"px) translateY("+ y +"px)";
     });
+}
+
+//////////////////////// Scrolling Animation ///////////////////////////////////////////
+document.addEventListener("scroll",scrolling);
+
+function scrolling(event){
+
+    console.log(document.documentElement.scrollTop);
+    const Number = Math.floor(document.documentElement.scrollTop/100);
+    console.log(typeof Number + Number);
+
+switch(true){
+    case (Number>10 && Number<18):
+        $(".skills").removeClass('animation');
+        $(".skills").addClass('animationx');
+        $(".profile").removeClass('animationx');
+        $(".profile").addClass('animation');
+        break;
+
+   
+    case (Number>4 && Number<10):
+                $(".skills").addClass('animation');
+                $(".skills").removeClass('animationx');
+                $(".profile").addClass('animationx');
+                $(".profile").removeClass('animation');
+                break;
+  case ( Number>18):
+                    $(".skills").addClass('animation');
+                    $(".skills").removeClass('animationx');
+                    // $(".profile").removeClass('animationx');
+                    // $(".profile").addClass('animation');
+                    break;
+    case (Number<4):
+                        // $(".skills").addClass('animation');
+                        // $(".skills").removeClass('animationx');
+                        $(".profile").removeClass('animationx');
+                        $(".profile").addClass('animation');
+                        break;
+
+}
+
+    // if(Number > 10 && Number < 18){
+    //     $(".skills").removeClass('animation');
+    //     $(".skills").addClass('animationx');
+    
+    // }else{
+    
+    //     $(".skills").addClass("animation");
+    //     $(".skills").removeClass('animationx');
+    // }
+    
 }
